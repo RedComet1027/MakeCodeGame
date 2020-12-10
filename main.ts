2aed70bdfa1639e0c9fd5061cc5107c29f8da901
RedComet1027 <47207846+RedComet1027@users.noreply.github.com>
2020-12-10 15:16:40 +0800

    Add createPrincess() and game win condition

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
+/**
+ * Next:
+ * 
+ * - Place coins evenly
+ * 
+ * Done:
+ * 
+ * - Add game win condition
+ * 
+ * - Lost live when hit balls
+ * 
+ * - Get points when hit coins
+ */
+function createPrincess () {
+    Princess = sprites.create(img`
+        . . . . . f f f f . . . . . 
+        . . . f f 5 5 5 5 f f . . . 
+        . . f 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 f . 
+        . f 5 5 5 d b b d 5 5 5 f . 
+        f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+        f 5 5 c c 4 4 4 4 c c 5 5 f 
+        f b b f b f 4 4 f b f b b f 
+        f b b 4 1 f d d f 1 4 b b f 
+        . f b f d d d d d d f b f . 
+        . f e f e 4 4 4 4 e f e f . 
+        . e 4 f 6 9 9 9 9 6 f 4 e . 
+        . 4 d c 9 9 9 9 9 9 c d 4 . 
+        . 4 f b 3 b 3 b 3 b b f 4 . 
+        . . f f 3 b 3 b 3 3 f f . . 
+        . . . . f f b b f f . . . . 
+        `, SpriteKind.Player)
+    Princess.setFlag(SpriteFlag.StayInScreen, true)
+    Princess.setPosition(80, 10)
+    controller.moveSprite(Princess)
+    info.setLife(3)
+}
 function createBalls () {
     EnemyBall_1 = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -41,6 +78,22 @@ function createBalls () {
     EnemyBall_2.x = randint(10, scene.screenWidth() - 10)
     EnemyBall_2.y = randint(10, scene.screenHeight() - 10)
     EnemyBall_2.setVelocity(-50, 50)
+}
+function createCoins () {
+    for (let index = 0; index < 10; index++) {
+        GoldCoin = sprites.create(img`
+            . . . b b b . . 
+            . . b 5 5 5 b . 
+            . b 5 d 3 d 5 b 
+            . b 5 1 5 3 5 b 
+            . c d 1 5 3 5 c 
+            . c d d 1 d 5 c 
+            . . f d d d f . 
+            . . . f f f . . 
+            `, SpriteKind.Food)
+        GoldCoin.x = randint(10, scene.screenWidth() - 10)
+        GoldCoin.y = randint(10, scene.screenHeight() - 10)
+    }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     music.magicWand.play()
@@ -86,60 +139,18 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     EnemyBall_2.destroy()
     createBalls()
 })
-/**
- * Next:
- * 
- * - To do more features
- * 
- * Done:
- * 
- * - Lost live when hit balls
- * 
- * - Get points when hit coins
- */
+let GoldCoin: Sprite = null
 let EnemyBall_2: Sprite = null
 let EnemyBall_1: Sprite = null
-let GoldCoin: Sprite = null
 let Princess: Sprite = null
 effects.starField.startScreenEffect()
-Princess = sprites.create(img`
-    . . . . . f f f f . . . . . 
-    . . . f f 5 5 5 5 f f . . . 
-    . . f 5 5 5 5 5 5 5 5 f . . 
-    . f 5 5 5 5 5 5 5 5 5 5 f . 
-    . f 5 5 5 d b b d 5 5 5 f . 
-    f 5 5 5 b 4 4 4 4 b 5 5 5 f 
-    f 5 5 c c 4 4 4 4 c c 5 5 f 
-    f b b f b f 4 4 f b f b b f 
-    f b b 4 1 f d d f 1 4 b b f 
-    . f b f d d d d d d f b f . 
-    . f e f e 4 4 4 4 e f e f . 
-    . e 4 f 6 9 9 9 9 6 f 4 e . 
-    . 4 d c 9 9 9 9 9 9 c d 4 . 
-    . 4 f b 3 b 3 b 3 b b f 4 . 
-    . . f f 3 b 3 b 3 3 f f . . 
-    . . . . f f b b f f . . . . 
-    `, SpriteKind.Player)
-Princess.setFlag(SpriteFlag.StayInScreen, true)
-Princess.setPosition(80, 10)
-controller.moveSprite(Princess)
-info.setLife(3)
-for (let index = 0; index < 10; index++) {
-    GoldCoin = sprites.create(img`
-        . . . b b b . . 
-        . . b 5 5 5 b . 
-        . b 5 d 3 d 5 b 
-        . b 5 1 5 3 5 b 
-        . c d 1 5 3 5 c 
-        . c d d 1 d 5 c 
-        . . f d d d f . 
-        . . . f f f . . 
-        `, SpriteKind.Food)
-    GoldCoin.x = randint(10, scene.screenWidth() - 10)
-    GoldCoin.y = randint(10, scene.screenHeight() - 10)
-}
+createPrincess()
+createCoins()
 createBalls()
 game.onUpdateInterval(500, function () {
     move(EnemyBall_1)
     move(EnemyBall_2)
+    if (info.score() == 1000) {
+        game.over(true, effects.confetti)
+    }
 })
